85e55d08f972c9bec54dbbcde0cb18c36048903d
DohunHyun <dohun.hyun@gmail.com>
2024-04-25 11:42:15 +0900

    front express 1차 완성

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const communityRoutes = require("./routes/communityRoutes"); 
+// const path = require("path");
+
+const app = express();
+const port = 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+app.use(communityRoutes);
+
+app.listen(port, () => {
+    console.log(`Community app listening on port ${port}`);
+});
+
---
+++ controllers/communityController.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const path = require('path');
+// const communityRoutes = require("../routes/communityRoutes"); 
+
+// const controllers = express.Router();
+const publicPath = path.join(__dirname, '../public');
+
+module.exports = {
+    getLogin(req, res) {
+        res.sendFile(path.join(publicPath, 'html/login.html'));
+    },
+    getSignin(req, res) {
+        res.sendFile(path.join(publicPath, 'html/signin.html'));
+    },
+    getProfileEdit(req, res) {
+        res.sendFile(path.join(publicPath, 'html/profileEdit.html'));
+    },
+    getPasswordEdit(req, res) {
+        res.sendFile(path.join(publicPath, 'html/passwordEdit.html'));
+    },
+    getBoard(req, res) {
+        res.sendFile(path.join(publicPath, 'html/board.html'));
+    },
+    getPostWrite(req, res) {
+        res.sendFile(path.join(publicPath, 'html/postWrite.html'));
+    },
+    getPostDetail(req, res) {
+        res.sendFile(path.join(publicPath, 'html/postDetail.html'));
+    },
+    getPostEdit(req, res) {
+        res.sendFile(path.join(publicPath, 'html/postEdit.html'));
+    },
+};
